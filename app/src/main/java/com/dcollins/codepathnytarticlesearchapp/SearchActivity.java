2f8bdb5a22608ac29acbf6c85e651570346bc12d
Derek P. Collins <dcollins@netflix.com>
2017-03-19 19:40:00 -0700

    Making successful NYT API call

--- app/src/main/java/com/dcollins/codepathnytarticlesearchapp/SearchActivity.java
+++ app/src/main/java/com/dcollins/codepathnytarticlesearchapp/SearchActivity.java
@@ -2,11 +2,19 @@ package com.dcollins.codepathnytarticlesearchapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
-import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -30,6 +38,20 @@ public class SearchActivity extends AppCompatActivity {
     public void onArticleSearch(View view) {
         String query = etQuery.getText().toString();
 
-        Toast.makeText(this, "Searching for " + query, Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, "Searching for " + query, Toast.LENGTH_LONG).show();
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+        RequestParams params = new RequestParams();
+        params.put("api-key", "dd1cd48f5049425a9d87d7720f87beb7");
+        params.put("page", 0);
+        params.put("q", query);
+
+        client.get(url, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d("DEBUG", response.toString());
+            }
+        });
     }
 }
