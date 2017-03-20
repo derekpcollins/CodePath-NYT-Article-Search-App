c7e0465a1c13989159902091b2ce5a14d5e68def
Derek P. Collins <dcollins@netflix.com>
2017-03-19 19:29:49 -0700

    Basic views in place

--- app/src/main/java/com/dcollins/codepathnytarticlesearchapp/SearchActivity.java
+++ app/src/main/java/com/dcollins/codepathnytarticlesearchapp/SearchActivity.java
@@ -1,13 +1,35 @@
 package com.dcollins.codepathnytarticlesearchapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class SearchActivity extends AppCompatActivity {
+
+    EditText etQuery;
+    GridView gvResults;
+    Button btnSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        setupViews();
+    }
+
+    public void setupViews() {
+        etQuery = (EditText) findViewById(R.id.etQuery);
+        gvResults = (GridView) findViewById(R.id.gvResults);
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+    }
+
+    public void onArticleSearch(View view) {
+        String query = etQuery.getText().toString();
+
+        Toast.makeText(this, "Searching for " + query, Toast.LENGTH_LONG).show();
     }
 }
