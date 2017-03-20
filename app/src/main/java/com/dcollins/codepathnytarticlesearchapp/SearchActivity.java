8a09b9194dae71ab9d512e09bc776f7336ef72e8
Derek P. Collins <dcollins@netflix.com>
2017-03-19 18:56:38 -0700

    Initial commit

---
+++ app/src/main/java/com/dcollins/codepathnytarticlesearchapp/SearchActivity.java
@@ -0,0 +1,13 @@
+package com.dcollins.codepathnytarticlesearchapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
